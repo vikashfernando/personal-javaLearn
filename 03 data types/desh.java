999714fd29d5d950aba06ad25104bcf6e0d96e4f
vikashfernando <vikashdeshitha@gmail.com>
2025-11-28 17:55:10 +0530

    l2 done

---
+++ 03 data types/desh.java
@@ -0,0 +1,21 @@
+public class desh{
+    public static void main(String[] args){
+
+        //main data types
+
+        String name="vikash";
+        int age=22;
+        double weight=78.54;
+        char gender='m';
+        boolean isBlack=true;
+
+        //printing these data
+
+        System.out.println("name: "+name);
+        System.out.println("age: "+age);
+        System.out.println("weight: "+weight);
+        System.out.println("gender: "+gender);
+        System.out.println("isBlack "+isBlack);
+
+    }
+}
