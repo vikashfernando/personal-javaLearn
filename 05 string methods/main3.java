2206d9701021639e1fd2ca60a50c861c86c062e1
vikashfernando <vikashdeshitha@gmail.com>
2025-11-30 07:06:04 +0530

    l5 done

--- 05 string methods/main3.java
+++ 05 string methods/main3.java
@@ -12,7 +12,7 @@ public class main3{
         System.out.println("*****************************");
 
 
-        //String methodss
+        //String methods
             //1 length of a string
             //2 index of a letter
             //3 convert to uppercase
