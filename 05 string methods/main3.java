91fcecc2c1606dc950cf949a2acfc9925c61e340
vikashfernando <vikashdeshitha@gmail.com>
2025-11-30 06:12:27 +0530

    l5 done

---
+++ 05 string methods/main3.java
@@ -0,0 +1,28 @@
+public class main3{
+    public static void main(String[] args){
+
+        String fName="vikash";
+        String lName="fernando";
+
+        String fullName=fName+lName;
+        String sentenceToDisplay="my name is "+fullName+" and my last name is "+lName;
+
+        System.out.println(fullName);
+        System.out.println(sentenceToDisplay);
+        System.out.println("*****************************");
+
+
+        //String methods
+            //1 length of a string
+            //2 index of a letter
+            //3 convert to uppercase
+            //4 convert to lowercase
+
+
+        System.out.println(fullName.length()); //1
+        System.out.println(fullName.indexOf("i")); //2
+        System.out.println(fullName.toUpperCase()); //3
+        System.out.println(fullName.toLowerCase()); //4
+
+    }
+}
