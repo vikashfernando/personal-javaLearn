ec13df1a353894169f51153814ed6a4ef2342310
vikashfernando <vikashdeshitha@gmail.com>
2025-12-01 06:14:30 +0530

    L6 done

---
+++ 06 escape sequences/Main4.java
@@ -0,0 +1,23 @@
+public class Main4{
+    public static void main(String[] args){
+
+        // 1    /b     (backspace)
+        // 2    /t     (tab)
+        // 3    /n     (new line)
+        // 4    /      (escape)
+        
+        System.out.println("hello there, how are yo\b\bu ");
+
+        System.out.println("suger:\t500g");
+        System.out.println("potatoes:\t2kg");
+        System.out.println("apples:\t5q");
+
+        System.out.println("\nmy name is vikash\nmy name is deshitha\nmy name is fernando");
+
+        System.out.println("he said \"my name is vikash\" and leaved....");
+
+
+
+
+    }
+}
