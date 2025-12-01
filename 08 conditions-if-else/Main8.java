949aa822e38aac0b1cff6d19a69f14cf4c0e662b
vikashfernando <vikashdeshitha@gmail.com>
2025-12-01 07:30:14 +0530

    L8 done

---
+++ 08 conditions-if-else/Main8.java
@@ -0,0 +1,38 @@
+public class Main8{
+    public static void main(String[] args){
+
+        // 1 if
+        // 2 if-else
+        // 3 if-elif-else
+
+
+        String name="vikash";
+
+        //1
+        if(name=="vikash"){
+        }System.out.println("user found");
+
+        //2
+        if(name=="apoo"){
+            System.out.println("winner");
+        }else{
+            System.out.println("loser");
+        }
+
+
+
+        //3
+        String name1="vikash";
+
+        if(name1=="nelli"){
+            System.out.println("A+");
+        }else if(name1=="apoo"){
+            System.out.println("B+");
+        }else if(name1=="vikash"){
+            System.out.println("C+");
+        }else{
+            System.out.println("good luck");
+        }
+
+    }
+}
