9fb933c062ecf963404c27497510bdc81c676a59
vikashfernando <vikashdeshitha@gmail.com>
2025-12-07 07:11:40 +0530

    l11 done

--- 11 loops do-while/Main11.java
+++ 11 loops do-while/Main11.java
@@ -1,2 +1,35 @@
-public class Main11 {
+//do-while-loops
+
+public class Main11{
+    public static void main(String[] args){
+
+        //eg-1
+        int i=0;
+        do{
+            System.out.println(i);
+            i++;
+        }while(i<=10);
+
+        //eg2
+
+        i=20;
+        do{
+            System.out.println(i);
+            i--;
+        }while(i>=5);
+
+
+        /*
+
+        ? difference while & do-while loops
+            while
+                It checks the condition first, then runs the code.
+                If the condition is false first time, the code will not run at all.
+            do-while
+                It runs the code first, then checks the condition.
+                The code runs at least one time, even if the condition is false.
+        */
+
+
+    }
 }
