81e90766be735fef785a8256d7957f703fade21e
vikashfernando <vikashdeshitha@gmail.com>
2025-12-07 06:58:58 +0530

    l10 done

--- 10 loops while/Main10.java
+++ 10 loops while/Main10.java
@@ -1,7 +1,26 @@
 public class Main10{
     public static void main(String[] args){
-        System.out.println("hello world");
+
+        //print numbers 1-9(do somthing for ptimes)
+        int i=1;
+        while(i<10){
+            System.out.println(i);
+            i++;
+        }
+
+        //print numbers 0-12(do somthing for 13times)
+        i=0;
+        while(i<=12){
+            System.out.println(i);
+            i++;
+        }
+
+
+        //print numbers 20-5
+        i=20;
+        while(i>=5){
+            System.out.println(i);
+            i--;
+        }
     }
 }
-
-
