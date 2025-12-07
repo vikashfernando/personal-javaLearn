01c7b4478f1c24c2514c486a2cca02380030f90b
vikashfernando <vikashdeshitha@gmail.com>
2025-12-07 07:21:30 +0530

    l12 done

--- 12 loops for/Main12.java
+++ 12 loops for/Main12.java
@@ -1,2 +1,21 @@
-public class Main12 {
+public class Main12{
+    public static void main(String args[]){
+
+        //eg1
+        for(int i=0;i<=10;i++){       //start value,condition,increase/decrease
+            System.out.println(i);
+        }
+
+        
+        /*
+        ? why while loop and for loop, and why we have two...
+            for
+                Use it when you know how many times you want to run the loop
+                eg-Print numbers 1 to 10
+            while
+                Use it when you do not know how many times the loop should run
+                eg-Repeat until user types "exit"
+         */
+
+    }
 }
