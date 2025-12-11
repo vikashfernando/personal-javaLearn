54b94f95b5afa372cdbb40512a153f29e0df1d7d
vikashfernando <vikashdeshitha@gmail.com>
2025-12-11 06:53:33 +0530

    corrected file directory

---
+++ gui-swing/1/src/Main.java
@@ -0,0 +1,18 @@
+/*
+What is java swing
+
+
+ */
+
+import javax.swing.*;    //import swing
+
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+
+        JFrame f=new JFrame("1st");     //create frame
+        f.setSize(400,300);               //create frame size
+        f.setVisible(true);               //frame visibility turned on
+
+    }
+}
